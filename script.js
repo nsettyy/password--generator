eb14b8dc3b390033f77e06f83920166f69885278
nsetty <nsett7388@gmail.com>
2023-10-16 21:48:49 -0400

    final changes

--- script.js
+++ script.js
@@ -1,22 +1,82 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0,];
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var specialCharacters = ["!", "@", "#", "$", "%", "&"];
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var uppercase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var lowercase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
-var choiceArray = []
+var choiceArray = [];
 
 function generatePassword() {
-
   //saves the typed in password lengeth as a integer
-  var length = parseInt(window.prompt ("Please choose the length of your password. Must be atleast 8 characters and no longer than 128 charcters."));
-  if (length >= 8 && length <= 128 ) {
+  var length = parseInt(
+    window.prompt(
+      "Please choose the length of your password. Must be atleast 8 characters and no longer than 128 charcters."
+    )
+  );
+  if (length >= 8 && length <= 128) {
     //runs the function again is a number is typed in that is lower than 8 or greater than 128
-  } else { window.alert ("Please choose a charcter length greater than 8 characters and less than 128 charcters.") 
-  return generatePassword();
-  } 
+  } else {
+    window.alert(
+      "Please choose a charcter length greater than 8 characters and less than 128 charcters."
+    );
+    return generatePassword();
+  }
 
   //adds uppercase characters to password if "OK" is clicked
   if (confirm("Would you like your password to have uppercase characters?")) {
@@ -37,7 +97,7 @@ function generatePassword() {
   if (confirm("Would you like your password to have special characters?")) {
     choiceArray = choiceArray.concat(specialCharacters);
   }
-  
+
   //the code that picks the chcraters rnadomly for the password
   var password = "";
   for (var i = 0; i < length; i++) {
@@ -48,15 +108,13 @@ function generatePassword() {
   return password;
 }
 
-  // Write password to the #password input
+// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
